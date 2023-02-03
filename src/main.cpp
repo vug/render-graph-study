c7ea8409e6b0804704e239367819db80c5100753
Ugur Guney <ugurguney@gmail.com>
2023-02-02 23:49:21 -0500

    Use ws::Shader. simple triangle without vertex buffer shader.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <Shader.h>
 #include <Workshop.h>
 
 #include <glad/gl.h>
@@ -14,10 +15,40 @@
 
 #include <iostream>
 
-int main() {
+int main()
+{
   ws::Workshop workshop{800, 600, "Workshop App"};
 
-  while (!workshop.shouldStop()) {
+  const char *vertexShader = R"(
+#version 300 es
+#extension GL_EXT_separate_shader_objects : enable
+precision mediump float;
+
+layout (location = 0) out vec3 fragColor;
+
+vec2 positions[3] = vec2[](vec2 (0.0, -0.5), vec2 (0.5, 0.5), vec2 (-0.5, 0.5));
+vec3 colors[3] = vec3[](vec3 (1.0, 0.0, 0.0), vec3 (0.0, 1.0, 0.0), vec3 (0.0, 0.0, 1.0));
+void main ()
+{
+	gl_Position = vec4 (positions[gl_VertexID], 0.0, 1.0);
+	fragColor = colors[gl_VertexID];
+}
+  )";
+
+  const char *fragmentShader = R"(
+#version 300 es
+#extension GL_EXT_separate_shader_objects : enable
+precision mediump float;
+
+layout (location = 0) in vec3 fragColor;
+layout (location = 0) out vec4 outColor;
+
+void main () { outColor = vec4 (fragColor, 1.0); }
+  )";
+  ws::Shader shader{vertexShader, fragmentShader};
+
+  while (!workshop.shouldStop())
+  {
     workshop.beginFrame();
 
     ImGui::Begin("Main");
