95c4f238cb5209461c310361e38be85405b4367b
Ugur Guney <ugurguney@gmail.com>
2023-02-04 18:51:04 -0500

    refactor out `makeTriangleShader`

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@
 
 #include <iostream>
 
+ws::Shader makeTriangleShader();
+
 int main()
 {
   ws::Workshop workshop{800, 600, "Workshop App"};
@@ -50,36 +52,10 @@ layout (location = 0) out vec4 outColor;
 void main () { 
   outColor = vec4(uv.x, uv.y, 0, 1.0); 
 }
-  )";  
-
-  const char *triangleVertexShader = R"(
-#version 300 es
-#extension GL_EXT_separate_shader_objects : enable
-precision mediump float;
-
-layout (location = 0) out vec3 fragColor;
-
-vec2 positions[3] = vec2[](vec2 (0.0, -0.5), vec2 (0.5, 0.5), vec2 (-0.5, 0.5));
-vec3 colors[3] = vec3[](vec3 (1.0, 0.0, 0.0), vec3 (0.0, 1.0, 0.0), vec3 (0.0, 0.0, 1.0));
-void main ()
-{
-	gl_Position = vec4 (positions[gl_VertexID], 0.0, 1.0);
-	fragColor = colors[gl_VertexID];
-}
   )";
 
-  const char *triangleFragmentShader = R"(
-#version 300 es
-#extension GL_EXT_separate_shader_objects : enable
-precision mediump float;
-
-layout (location = 0) in vec3 fragColor;
-layout (location = 0) out vec4 outColor;
-
-void main () { outColor = vec4 (fragColor, 1.0); }
-  )";
-  ws::Shader triangleShader{triangleVertexShader, triangleFragmentShader};
   ws::Shader fullScreenShader{fullScreenVertexShader, fullScreenFragmentShader};
+  ws::Shader triangleShader = makeTriangleShader();
 
   while (!workshop.shouldStop())
   {
@@ -108,4 +84,35 @@ void main () { outColor = vec4 (fragColor, 1.0); }
   }
 
   return 0;
+}
+
+ws::Shader makeTriangleShader()
+{
+  const char *triangleVertexShader = R"(
+#version 300 es
+#extension GL_EXT_separate_shader_objects : enable
+precision mediump float;
+
+layout (location = 0) out vec3 fragColor;
+
+vec2 positions[3] = vec2[](vec2 (0.0, -0.5), vec2 (0.5, 0.5), vec2 (-0.5, 0.5));
+vec3 colors[3] = vec3[](vec3 (1.0, 0.0, 0.0), vec3 (0.0, 1.0, 0.0), vec3 (0.0, 0.0, 1.0));
+void main ()
+{
+	gl_Position = vec4 (positions[gl_VertexID], 0.0, 1.0);
+	fragColor = colors[gl_VertexID];
+}
+  )";
+
+  const char *triangleFragmentShader = R"(
+#version 300 es
+#extension GL_EXT_separate_shader_objects : enable
+precision mediump float;
+
+layout (location = 0) in vec3 fragColor;
+layout (location = 0) out vec4 outColor;
+
+void main () { outColor = vec4 (fragColor, 1.0); }
+  )";
+  return ws::Shader{triangleVertexShader, triangleFragmentShader};
 }
